029e942ee91c8247152ff78d73a91f69005e0124
yanakin394 <yana.germatskaya@gmail.com>
2023-07-04 10:39:40 +0300

    popup function changed

--- assets/script/script.js
+++ assets/script/script.js
@@ -303,33 +303,29 @@ const jokeBtn = document.getElementById('joke');
 const popupClose = document.querySelector('.popup__close');
 const jokeText = document.querySelector('.popup__text');
 const divBody = document.getElementById('body');
-//функция для появления и закрытия попапа
-function popupFunc () {
-    jokeBtn.addEventListener('click', event => {        
-        event.preventDefault();
-        jokeApiReq ();
-        divBody.style.overflow = 'hidden';    //убираем 2й скролл
-        jokePopup.classList.add('open');
-        
-    })
-    popupClose.addEventListener('click', event => {
-        event.preventDefault();
-        divBody.style.overflow = '';
-        jokePopup.classList.remove('open');
-    })
-}
-popupFunc();
 
 //функция запроса шутки с апи
 async function jokeApiReq () {
-    try {
-        const apiKey = '39355f2c938446e9a0ac53c6bfcb3782';
-        let response = await fetch(`https://api.spoonacular.com/food/jokes/random?apiKey=${apiKey}`);
-        console.log(response);
-        let joke = await response.json();
-        jokeText.textContent = joke.text;
-    }
-    catch (error) {
-        jokeText.textContent = `Server is not responding: ${error.message}`;
-    }
+  try {
+      const apiKey = '39355f2c938446e9a0ac53c6bfcb3782';
+      let response = await fetch(`https://api.spoonacular.com/food/jokes/random?apiKey=${apiKey}`);
+      console.log(response);
+      let joke = await response.json();
+      jokeText.textContent = joke.text;
+  }
+  catch (error) {
+      jokeText.textContent = `Server is not responding: ${error.message}`;
+  }
 }
+//прослушка на кнопку и на закрытие попапа
+jokeBtn.addEventListener('click', event => {        
+  event.preventDefault();
+  jokeApiReq ();
+  divBody.style.overflow = 'hidden';    //убираем 2й скролл
+  jokePopup.classList.add('open');
+})
+popupClose.addEventListener('click', event => {
+  event.preventDefault();
+  divBody.style.overflow = '';
+  jokePopup.classList.remove('open');
+})
